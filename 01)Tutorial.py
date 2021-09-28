a80f86fd80388edaa485c2d9fc2a1f444115088d
DotReNJu <112303dh@naver.com>
2021-09-28 21:50:57 +0900

    add comment link

--- 01)Tutorial.py
+++ 01)Tutorial.py
@@ -1,4 +1,6 @@
 """comment
 #01 
 """
+#참고 사이트1 : http://pythonstudy.xyz/python/article/8-%EC%97%B0%EC%82%B0%EC%9E%90
+#참고 사이트2 : https://programmers.co.kr/learn/courses/2/lessons/178
 print("Hello world!")
