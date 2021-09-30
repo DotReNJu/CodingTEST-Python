a2a33d6c8b04de5c84f9b018d5e175a77bdea84d
DotReNJu <112303dh@naver.com>
2021-09-30 14:39:45 +0900

    programmers level1

--- m_01)programers.py
+++ m_01)programers.py
@@ -1,5 +1,6 @@
 #링크 https://programmers.co.kr/learn/courses/30/lessons/12917?language=python
-
+#다른사람 풀이
+#https://programmers.co.kr/learn/courses/30/lessons/12917/solution_groups?language=python
 """
 문제 : 문제 설명
 문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
@@ -11,19 +12,11 @@ str은 길이 1 이상인 문자열입니다.
 s	return
 "Zbcdefg"	"gfedcbZ"
 """
-
 #풀이
 def solution(s):
     #answer = sorted(s) 
     #answer= s.sort(reverse=True)
-
-
     answer = "".join(sorted(s,reverse=True))
     return answer
-
 print(solution("Zbcdefg"))
 
-
-
-#다른사람 풀이
-#https://programmers.co.kr/learn/courses/30/lessons/12917/solution_groups?language=python
