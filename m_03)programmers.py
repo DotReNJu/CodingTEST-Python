29430527e2a0364f087b1049aaf364d0119d8a62
DotReNJu <112303dh@naver.com>
2021-10-05 20:20:55 +0900

    programmers level1

--- m_03)programmers.py
+++ m_03)programmers.py
@@ -7,7 +7,6 @@ def solution(left, right):
         for j in range(1,i+1):
             if (i%j)==0:
                 arr.append(j)
-        print(arr)
         if len(arr)%2==0:
             answer=answer+i
         else:
