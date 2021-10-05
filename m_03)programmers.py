bfdf14a9a7cda4e61e3e9af17241c59c678705a0
Kim DongHun <38520071+DotReNJu@users.noreply.github.com>
2021-10-05 16:17:50 +0900

    programmers level1

--- m_03)programmers.py
+++ m_03)programmers.py
@@ -1,1 +1,15 @@
 #python code
+def solution(left, right):
+    answer=0
+    arr=[]
+    for i in range(left,right+1):
+        arr=[]
+        for j in range(1,i+1):
+            if (i%j)==0:
+                arr.append(j)
+        print(arr)
+        if len(arr)%2==0:
+            answer=answer+i
+        else:
+            answer=answer-i
+    return answer
