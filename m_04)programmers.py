34a8744084e71772b3ba307be6e82358ffa3efea
Kim DongHun <38520071+DotReNJu@users.noreply.github.com>
2021-10-14 15:35:18 +0900

    test

--- m_04)programmers.py
+++ m_04)programmers.py
@@ -40,7 +40,16 @@ def solution3(s):
     print(answer)
     return int(answer)
 
+num_dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+
+def solution4(s):
+    answer = s
+    for key, value in num_dic.items():
+        answer = answer.replace(key, value)
+    return int(answer)
+
 solution("123tzeroest123zero")
 solution2("123tzeroest123zero")
 
 solution3("123tzeroest123zero")
+solution4("123tzeroest123zero")
