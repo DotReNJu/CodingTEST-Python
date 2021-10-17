8368130e604f07614fd53061e245302441427f9f
Kim DongHun <38520071+DotReNJu@users.noreply.github.com>
2021-10-17 14:41:33 +0900

    code test

--- m_05)programmers.py
+++ m_05)programmers.py
@@ -1,6 +1,7 @@
-#문제 
+#문제 https://programmers.co.kr/learn/courses/30/lessons/12915?language=python3
 
-def solution(strings, n):
+
+def solution2(strings, n):
     answer = sorted(sorted(strings),key=lambda x:x[n])
     print(answer)
     return answer
