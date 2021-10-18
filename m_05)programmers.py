e54f5b6c3857cebb20ab51c43dd13b1725fe0c9b
Kim DongHun <38520071+DotReNJu@users.noreply.github.com>
2021-10-18 19:23:20 +0900

    test

--- m_05)programmers.py
+++ m_05)programmers.py
@@ -1,5 +1,6 @@
 #문제 https://programmers.co.kr/learn/courses/30/lessons/12915?language=python3
-
+#n번째 문자를 기준으로 정렬.
+#기본적으로 오름차순 정렬.
 
 def solution2(strings, n):
     answer = sorted(sorted(strings),key=lambda x:x[n])
