c960c15ca36a69a9b5f06f7e51ba926982fa2c92
DotReNJu <112303dh@naver.com>
2021-10-19 19:59:58 +0900

    not solve

--- m_05)programmers.py
+++ m_05)programmers.py
@@ -2,7 +2,52 @@
 #n번째 문자를 기준으로 정렬.
 #기본적으로 오름차순 정렬.
 
+testcase=[[
+    "sua",
+    "bed",
+    "car",
+    "sux",
+    "suc",
+    "sud",
+    "sug",
+    "suh",
+    "sul",
+    "sum",
+    "sub",
+    "sut",
+    "suw",
+    "sue",
+    "suk",
+    "suq",
+    "suz",
+    "suj",
+    "suo",
+    "sup",
+    "aug",
+    "but"],
+    1]
+
+def solution(strings, n):
+    answer = []
+    strings=sorted(strings)
+    tmp='';
+    
+    for s1 in range(len(strings)-1):
+        for s2 in range(s1+1,len(strings)):
+            if strings[s1][n]>strings[s2][n]:
+                tmp=strings[s1]
+                strings[s1]=strings[s2]
+                strings[s2]=tmp
+                tmp=''
+                print(strings)
+    answer=strings
+
+    print(answer)
+    return answer
+
 def solution2(strings, n):
     answer = sorted(sorted(strings),key=lambda x:x[n])
     print(answer)
     return answer
+
+solution(testcase)
